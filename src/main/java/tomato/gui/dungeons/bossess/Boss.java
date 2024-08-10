28438597319b0f4392ea389ca177561597ff3bfe
IDjinn <42049518+IDjinn@users.noreply.github.com>
2024-08-10 14:15:48 -0300

    versioning jar name & jdk 22

--- src/main/java/tomato/gui/dungeons/bossess/Boss.java
+++ src/main/java/tomato/gui/dungeons/bossess/Boss.java
@@ -1,14 +1,9 @@
 package tomato.gui.dungeons.bossess;
 
-import sun.awt.Mutex;
 
 public class Boss {
-    private final Mutex mutex;
     
     private String name;
     private long health;
 
-    public Boss() {
-        this.mutex = new Mutex();
-    }
 }
